82bdd143d377e267ef3fed2eed6a1214478b09b7
sonle1601 <71656736+sonle1601@users.noreply.github.com>
2021-03-12 16:46:35 +0100

    finish website( collection, custom services, blogs, footer)

--- assets/js/index.js
+++ assets/js/index.js
@@ -84,3 +84,25 @@ mainSlider.onmouseout = function() {
 
 
 
+// Scroll to top 
+var scrollBtn = document.querySelector('.scroll-top')
+// console.log(scrollBtn)
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    scrollBtn.style.opacity = "1";
+    scrollBtn.style.visibility = "visible";
+  } else {
+    scrollBtn.style.opacity = "0";
+    scrollBtn.style.visibility = "hidden";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollBtn.onclick = function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
